71f7584d004ec2b43951c0884463fe800467e8d4
3sims <telcos.twins0a@icloud.com>
2026-04-30 18:51:28 +0200

    nest build precompile fix

--- apps/api/webpack-serverless.js
+++ apps/api/webpack-serverless.js
@@ -5,4 +5,15 @@ module.exports = (options) => ({
     filename: 'serverless.js',
     libraryTarget: 'commonjs2',
   },
-})
+  externals: [
+    function ({ request }, callback) {
+      if (request && request.startsWith('@autocrm/')) {
+        return callback();
+      }
+      if (request && !request.startsWith('.') && !request.startsWith('/')) {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    },
+  ],
+});
